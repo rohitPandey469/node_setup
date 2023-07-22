103196cf7798ef41de88bda653066adb7424424b
rohitPandey469 <rd8614196@gmail.com>
2023-07-22 16:45:03 +0530

    site link added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["https://react-auth-signup-login.netlify.app"];
+const allowedOrigins = [
+    "https://react-auth-signup-login.netlify.app",
+    "https://react-auth-signup-login.onrender.com"
+];
 
 module.exports = allowedOrigins;

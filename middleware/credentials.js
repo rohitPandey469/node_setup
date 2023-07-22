194818c7c38ceae833bbbce128b57b94de09b1a3
rohitPandey469 <rd8614196@gmail.com>
2023-07-22 17:11:50 +0530

    Update credentials.js

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -4,7 +4,7 @@ const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", true);
-    // res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://react-auth-signup-login.onrender.com");
   }
   next();
 };

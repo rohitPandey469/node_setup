59a11e0393ad12da8d9ed8998b76a95e909a44a0
rohitPandey469 <rd8614196@gmail.com>
2023-07-22 12:05:24 +0530

    Commited

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,4 +1,4 @@
-const allowedOrigins=require('./allowedOrigins');
+const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
   origin: (origin, callback) => {
@@ -8,7 +8,8 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"));
     }
   },
+  credentials: true,
   optionsSuccessStatus: 200,
 };
 
-module.exports=corsOptions;
+module.exports = corsOptions;
--- middleware/credentials.js
+++ middleware/credentials.js
@@ -3,8 +3,7 @@ const allowedOrigins = require("../config/allowedOrigins");
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", true);
+    res.header("Access-Control-Allow-Origin", "*");
   }
   next();
 };

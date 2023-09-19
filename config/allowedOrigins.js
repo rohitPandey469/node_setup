fea93ba33b33e237f3f99b2a0e63cd42ee313f98
rohitPandey469 <rd8614196@gmail.com>
2023-09-19 12:33:48 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     "https://react-auth-signup-login.netlify.app",
     "https://react-auth-signup-login.onrender.com",
     "http://localhost:5173",
-    "https://ayurveda-to-the-rescue.netlify.app"
+    "https://ayurveda-to-the-rescue.netlify.app",
+    "https://ayurhelp.netlify.app"
 ];
 
 module.exports = allowedOrigins;
